434b3de2cee8747fd6f95898a146575a5e859e9c
sanskar <sanskar.verma90@gmail.com>
2023-04-28 15:20:44 +1000

    Add setters in Card class

--- src/main/java/com/learn/sanskar/udemy/cards/model/Card.java
+++ src/main/java/com/learn/sanskar/udemy/cards/model/Card.java
@@ -30,4 +30,43 @@ public class Card {
     @Column(name = "create_dt")
     private Date createDt;
 
+    public Card cardId(Integer cardId) {
+        this.cardId = cardId;
+        return this;
+    }
+
+    public Card customerId(Integer customerId) {
+        this.customerId = customerId;
+        return this;
+    }
+
+    public Card cardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+        return this;
+    }
+
+    public Card cardType(String cardType) {
+        this.cardType = cardType;
+        return this;
+    }
+
+    public Card totalLimit(Integer totalLimit) {
+        this.totalLimit = totalLimit;
+        return this;
+    }
+
+    public Card amountUsed(Integer amountUsed) {
+        this.amountUsed = amountUsed;
+        return this;
+    }
+
+    public Card availableAmount(Integer availableAmount) {
+        this.availableAmount = availableAmount;
+        return this;
+    }
+
+    public Card createDt(Date createDt) {
+        this.createDt = createDt;
+        return this;
+    }
 }
